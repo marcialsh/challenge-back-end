d7ba441d7e82c65198cfc25ae9f14507a7d507e7
Guilherme Marcial <guilherme.marcial.felipe@gmail.com>
2018-10-01 03:20:00 -0400

    Implementando estrutura do database

---
+++ src/BlogStarWars.Database/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AgroLowCost.Database
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static IServiceProvider CreateServices()
+        {
+            /*var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.GetFullPath("../BlogStarWars.Api"))
+                .AddJsonFile("appsettings.Development.json");*/
+
+            //var configuration = builder.Build();
+
+            var migrationAssemblies = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .Where(e => e.IsClass && e.Namespace == "BlogStarWars.Database.Migrations" &&
+                            e.BaseType == typeof(Migration))
+                .Select(e => e.Assembly).ToArray();
+
+            /*var connectionString = configuration
+                .GetConnectionString(
+                    RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+                        ? "AgroLowCostMac"
+                        : "AgroLowCostLinux");*/
+
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSQLite()
+                    // Set the connection string
+                    .WithGlobalConnectionString("Data Source=BlogStarWars.db;")
+                    // Define the assembly containing the migrations
+                    .ScanIn(migrationAssemblies).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary/>
+        /// Update the database
+        /// 
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+    }
+}
